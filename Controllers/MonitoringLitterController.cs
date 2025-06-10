63244ab797aecbd53d7cbcbc47c7f0735344dec1
zlaticaa <cstudyspace@gmail.com>
2025-06-10 13:21:04 +0200

    Added project

---
+++ Controllers/MonitoringLitterController.cs
@@ -0,0 +1,117 @@
+﻿using HomeTry.Models;
+using HomeTry.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+
+namespace HomeTry.Controllers
+{
+    [ApiController]
+    [Route("litter")]
+    public class MonitoringLitterController : ControllerBase
+    {
+        private readonly LitterRepository _litterRepository;
+        private readonly ILogger<MonitoringLitterController> _logger;
+
+        public MonitoringLitterController(LitterRepository litterRepository, ILogger<MonitoringLitterController> logger)
+        {
+            _litterRepository = litterRepository;
+            _logger = logger;
+        }
+
+        [HttpGet("today", Name = "today")]
+        //read all litter + weather records from a specific date
+        public async Task<IActionResult> Get([FromQuery] DateOnly? date, [FromQuery] int? cat)
+        {
+            if (date.HasValue && cat.HasValue)
+            {
+                DateTime startDateTime = date.Value.ToDateTime(TimeOnly.MinValue);
+                DateTime endDateTime = date.Value.ToDateTime(TimeOnly.MaxValue);
+
+                var data = await _litterRepository.ReadAsync(startDateTime, endDateTime, cat.Value);
+                return Ok(data);
+            }
+            if (date.HasValue)
+            {
+                DateTime startDateTime = date.Value.ToDateTime(TimeOnly.MinValue);
+                DateTime endDateTime = date.Value.ToDateTime(TimeOnly.MaxValue);
+
+                var data = await _litterRepository.ReadAsync(startDateTime, endDateTime);
+                return Ok(data);
+            }
+            if (cat.HasValue)
+            {
+                date = DateOnly.FromDateTime(DateTime.Now);
+
+                DateTime startDateTime = date.Value.ToDateTime(TimeOnly.MinValue);
+                DateTime endDateTime = date.Value.ToDateTime(TimeOnly.MaxValue);
+
+                var data = await _litterRepository.ReadAsync(startDateTime, endDateTime, cat.Value);
+                return Ok(data);
+            }
+            else
+            {
+                date = DateOnly.FromDateTime(DateTime.Now);
+                DateTime startDateTime = date.Value.ToDateTime(TimeOnly.MinValue);
+                DateTime endDateTime = date.Value.ToDateTime(TimeOnly.MaxValue);
+
+                var data = await _litterRepository.ReadAsync(startDateTime, endDateTime);
+                return Ok(data);
+            }
+        }
+
+        [HttpGet]
+        //read all litter + weather records between certain dates
+        public async Task<IActionResult> Get([FromQuery] DateOnly? beginDate, [FromQuery] DateOnly? endDate, [FromQuery] int? cat)
+        {
+            if (beginDate.HasValue && endDate.HasValue && cat.HasValue)
+            {
+                DateTime startDateTime = beginDate.Value.ToDateTime(TimeOnly.MinValue);
+                DateTime endDateTime = endDate.Value.ToDateTime(TimeOnly.MaxValue);
+
+                var data = await _litterRepository.ReadAsync(startDateTime, endDateTime, cat.Value);
+                return Ok(data);
+            }
+            if (beginDate.HasValue && endDate.HasValue)
+            {
+                DateTime startDateTime = beginDate.Value.ToDateTime(TimeOnly.MinValue);
+                DateTime endDateTime = endDate.Value.ToDateTime(TimeOnly.MaxValue);
+
+                var data = await _litterRepository.ReadAsync(startDateTime, endDateTime);
+                return Ok(data);
+            }
+            if (beginDate.HasValue && cat.HasValue)
+            {
+                DateTime startDateTime = beginDate.Value.ToDateTime(TimeOnly.MinValue);
+
+                var data = await _litterRepository.ReadAsync(startDateTime, cat.Value);
+                return Ok(data);
+            }
+            if (beginDate.HasValue)
+            {
+                DateTime startDateTime = beginDate.Value.ToDateTime(TimeOnly.MinValue);
+
+                var data = await _litterRepository.ReadAsync(startDateTime);
+                return Ok(data);
+            }
+            if (cat.HasValue)
+            {
+                var data = await _litterRepository.ReadAsync(cat.Value);
+                return Ok(data);
+            }
+            else
+            {
+                var data = await _litterRepository.ReadAsync();
+                return Ok(data);
+            }
+        }
+
+
+        //read a litter + weather record by litter id
+        [HttpGet("{id}", Name = "id")]
+        public async Task<ActionResult<Litter>> Get(Guid id)
+        {
+            var litter = await _litterRepository.ReadAsync(id);
+            return Ok(litter);
+        }
+    }
+}
---
+++ Controllers/SensoringLitterController.cs
@@ -0,0 +1,78 @@
+﻿using HomeTry.Models;
+using HomeTry.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
+using System.Text.Json;
+
+namespace HomeTry.Controllers
+{
+    [ApiController]
+    [Route("litter")]
+    public class SensoringLitterController : ControllerBase
+    {
+        private readonly LitterRepository _litterRepository;
+        private readonly HttpClient _httpClient;
+        private readonly string _apiKey;
+        private readonly ILogger<SensoringLitterController> _logger;
+
+        public SensoringLitterController(LitterRepository litterRepository, ILogger<SensoringLitterController> logger, IHttpClientFactory httpClientFactory, IConfiguration configuration)
+        {
+            _litterRepository = litterRepository;
+            _logger = logger;
+            _httpClient = httpClientFactory.CreateClient();
+            _apiKey = configuration["WeatherApiKey"];
+        }
+
+
+        //insert a new record
+        [HttpPost("manual", Name = "ManualUpload" )]
+        public async Task<ActionResult> AddManual(Litter litter)
+        {
+            litter.litter_id = Guid.NewGuid();
+            litter.weather.weather_id = Guid.NewGuid();
+            litter.weather_id = litter.weather.weather_id; ;
+
+            var createdWeatherForecast = await _litterRepository.InsertAsync(litter, litter.weather);
+            return CreatedAtRoute("ManualUpload", new { id = litter.litter_id }, litter);
+        }
+
+        [HttpPost("auto", Name = "AutoUpload")]
+        public async Task<ActionResult> AddAuto(Litter litter)
+        {
+            // Fetch weather info
+            string url = $"http://api.weatherapi.com/v1/current.json?key={_apiKey}&q={litter.location_latitude},{litter.location_longitude}";
+
+
+            HttpResponseMessage response = await _httpClient.GetAsync(url);
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine("Error: " + response.StatusCode);
+                Console.WriteLine("Request to weather API has failed");
+                return StatusCode((int)response.StatusCode, "Weather API failed.");
+            }
+
+            string content = await response.Content.ReadAsStringAsync();
+            dynamic data = JsonConvert.DeserializeObject<dynamic>(content);
+
+            // Prepare new weather + litter records
+            litter.litter_id = Guid.NewGuid();
+            litter.weather_id = Guid.NewGuid();
+
+            if (litter.weather == null)
+                litter.weather = new Weather(); // Ensure it's not null
+
+            litter.weather.weather_id = (Guid)litter.weather_id;
+            litter.weather.temperature_celsius = data.current?.temp_c;
+            litter.weather.humidity = data.current?.humidity;
+            litter.weather.conditions = data.current?.condition?.text;
+
+            // Insert
+            var createdWeatherForecast = await _litterRepository.InsertAsync(litter, litter.weather);
+
+            return CreatedAtRoute("AutoUpload", new { id = litter.litter_id }, litter);
+        }
+
+    }
+}
