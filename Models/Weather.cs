584d48dadc98c92f09f7b13b0285de2ff37cd0e6
zlaticaa <cstudyspace@gmail.com>
2025-06-10 13:03:00 +0200

    First Commit
    Added project & .gitignore

---
+++ Models/Weather.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HomeTry.Models
+{
+    public class Weather
+    {
+        [Key]
+        public Guid weather_id { get; set; }
+
+        [Required]
+        public double temperature_celsius { get; set; }
+
+        [Required]
+        public double humidity { get; set; }
+
+        [Required, MaxLength(20)]
+        public string conditions { get; set; }
+    }
+}
