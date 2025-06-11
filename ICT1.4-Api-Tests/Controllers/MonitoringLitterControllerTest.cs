41807fb440a468e4e7e44ef39a4407636f27798a
zlaticaa <cstudyspace@gmail.com>
2025-06-11 16:43:59 +0200

    Added first test class

---
+++ ICT1.4-Api-Tests/Controllers/MonitoringLitterControllerTest.cs
@@ -0,0 +1,13 @@
+using Moq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ICT1._4_Api_Tests;
+
+[TestClass]
+public class MonitoringLitterControllerTest
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
