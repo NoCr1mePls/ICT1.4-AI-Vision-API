c2fd4d13576c832bf5d3c03f90eaa680cf3c74a2
zlaticaa <cstudyspace@gmail.com>
2025-06-12 14:19:08 +0200

    added webapi project

---
+++ ICT1.4-AI-Vision-API/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace ICT1._4_AI_Vision_API
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
