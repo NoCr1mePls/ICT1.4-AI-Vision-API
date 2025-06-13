ae7ab33ea5728dc8d1708bff8846cca345846b7c
zlaticaa <cstudyspace@gmail.com>
2025-06-13 11:49:47 +0200

    added repo interface

--- ICT1.4-AI-Vision-API-Test/Controllers/MonitoringLitterControllerTest.cs
+++ ICT1.4-AI-Vision-API-Test/Controllers/MonitoringLitterControllerTest.cs
@@ -1,33 +1,35 @@
 using Moq;
 using Microsoft.AspNetCore.Mvc;
 using HomeTry.Controllers;
+using HomeTry.Interfaces;
 using HomeTry.Repositories;
 using HomeTry.Models;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace ICT1._4_AI_Vision_API_Test;
 
 [TestClass]
 public class MonitoringLitterControllerTest
 {
-    private Mock<LitterRepository> _mockRepo;
-    private Mock<AuthenticationService> _mockAuth;
+    private Mock<ILitterRepository> _mockRepo;
+    private Mock<ILogger<MonitoringLitterController>> _mockLogger;
     private MonitoringLitterController _controller;
 
     [TestInitialize]
     public void Setup()
     {
-        _mockRepo = new Mock<LitterRepository>();
-        _mockAuth = new Mock<AuthenticationService>();
-        _controller = new MonitoringLitterController(_mockRepo.Object, _mockAuth.Object);
+        _mockRepo = new Mock<ILitterRepository>();
+        _mockLogger = new Mock<ILogger<MonitoringLitterController>>();
+        _controller = new MonitoringLitterController(_mockRepo.Object, _mockLogger.Object);
     }
 
     [TestMethod]
     public async Task Get_ReturnsOkResult_WithJsonString()
     {
         // Arrange
-        var userId = "null";
+        var userId = new Guid();
         var jsonResult = "testJson";
-        _mockAuth.Setup(auth => auth.GetCurrentAuthenticatedUserID()).Returns(userId);
         _mockRepo.Setup(repo => repo.ReadAsync(userId)).ReturnsAsync(jsonResult);
         int? cat = 0;
 
@@ -45,7 +47,6 @@ public class MonitoringLitterControllerTest
     {
         // Arrange
         var userId = "testUserId";
-        _mockAuth.Setup(auth => auth.GetCurrentAuthenticatedUserID()).Returns(userId);
         _mockRepo.Setup(repo => repo.ReadAsync(userId)).ReturnsAsync((string)null);
 
         // Act
