2dc1989fb35e9f6dd353d3d75bcae59e67119e0d
zlaticaa <cstudyspace@gmail.com>
2025-06-16 12:08:36 +0200

    added 3 edge case unit tests

--- ICT1.4-AI-Vision-API-Test/Controllers/MonitoringLitterControllerTest.cs
+++ ICT1.4-AI-Vision-API-Test/Controllers/MonitoringLitterControllerTest.cs
@@ -58,6 +58,23 @@ public class MonitoringLitterControllerTest
     }
 
     [TestMethod]
+    public async Task GetOfDate_ReturnsOkRequest_WithLitterObject_WhenDateIsInRange()
+    {
+        // Arrange
+        IEnumerable<Litter?> returnLitter = new List<Litter?>();
+        DateOnly? date = new DateOnly(2025, 6, 15);
+        _mockRepo.Setup(repo => repo.ReadAsync(date.Value.ToDateTime(TimeOnly.MinValue), date.Value.ToDateTime(TimeOnly.MaxValue))).ReturnsAsync(returnLitter);
+
+        // Act
+        var result = await _controller.Get(date, null);
+
+        // Assert
+        var okResult = result as OkObjectResult;
+        Assert.IsNotNull(okResult);
+        Assert.AreEqual(returnLitter, okResult.Value);
+    }
+
+    [TestMethod]
     public async Task GetBetweenRange_ReturnsOkResult_WithLitterObject()
     {
         // Arrange
@@ -90,5 +107,41 @@ public class MonitoringLitterControllerTest
         // Assert
         var badRequestResult = result as BadRequestResult;
         Assert.IsNull(badRequestResult);
+    }
+
+    [TestMethod]
+    public async Task GetBetweenRange_ReturnsOkResult_WithEmptyLitterObject_WhenRangeIsOutOfDataRange()
+    {
+        // Arrange
+        IEnumerable<Litter?> returnLitter = null;
+        DateOnly? startDate = new DateOnly(1800, 1, 1);
+        DateOnly? endDate = new DateOnly(1900, 12, 31);
+        _mockRepo.Setup(repo => repo.ReadAsync(startDate.Value.ToDateTime(TimeOnly.MinValue), endDate.Value.ToDateTime(TimeOnly.MaxValue))).ReturnsAsync(returnLitter);
+
+        //Act
+        var result = await _controller.Get(startDate, endDate, null);
+
+        //Assert
+        var okResult = result as OkObjectResult;
+        Assert.IsNull(okResult.Value);
+        Assert.AreEqual(returnLitter, okResult.Value);
+    }
+
+    [TestMethod]
+    public async Task GetBetweenRange_ReturnsOkResult_WithLitterObject_WhenRangeEncompassesDataRange()
+    {
+        // Arrange
+        IEnumerable<Litter?> returnLitter = new List<Litter?>();
+        DateOnly? startDate = new DateOnly(1800, 1, 1);
+        DateOnly? endDate = new DateOnly(2200, 12, 31);
+        _mockRepo.Setup(repo => repo.ReadAsync(startDate.Value.ToDateTime(TimeOnly.MinValue), endDate.Value.ToDateTime(TimeOnly.MaxValue))).ReturnsAsync(returnLitter);
+
+        //Act
+        var result = await _controller.Get(startDate, endDate, null);
+
+        //Assert
+        var okResult = result as OkObjectResult;
+        Assert.IsNotNull(okResult);
+        Assert.AreEqual(returnLitter, okResult.Value);
     }
 }
